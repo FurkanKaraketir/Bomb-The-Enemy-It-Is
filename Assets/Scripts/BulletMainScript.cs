aeb0a65943024545f29ccb6f0d52e89b54282bb6
Furkan Karaketir <118582197+FurkanKaraketir@users.noreply.github.com>
2023-10-10 21:08:09 +0300

    UI Added

--- Assets/Scripts/BulletMainScript.cs
+++ Assets/Scripts/BulletMainScript.cs
@@ -8,11 +8,12 @@ public class BulletMainScript : MonoBehaviour
     public float speed;
     public bool follow = true;
     public GameObject plane;
+    public GameObject text;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -27,16 +28,29 @@ public class BulletMainScript : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 follow = false;
+                gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.right * speed;
 
             }
         }
-        else
-        {
-            transform.position = transform.position + (Vector3.right * (speed * Time.deltaTime));
 
+
+
+        
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.name == "Enemy")
+        {
+            text.SetActive(true);
         }
 
-
+        if (collision.gameObject.name == "Ground")
+        {
+            gameObject.SetActive(false);
+        }
 
 
     }
